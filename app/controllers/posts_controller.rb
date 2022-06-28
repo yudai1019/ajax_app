5580a52647424ef449f14eddc512c8f90e48d3ce
yudai1019 <yudaydayo@gmail.com>
2022-06-28 17:55:46 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.Post(content: params[:content])
+    post = Post.create(content: params[:content])
     render json:{ post: post }
   end
 end
